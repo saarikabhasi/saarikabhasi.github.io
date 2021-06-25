f21a8539020a4a2450e30c142e4e2f1620717927
Nair Saarika Bhasi <saarikabhasi@yahoo.com>
2021-06-25 13:15:49 -0400

    final-setup

--- js/skills.js
+++ js/skills.js
@@ -1,4 +1,22 @@
+const projects = {
+    "title":["the-Book-fair","chat-application","post-it","wikipedia","social-network"]
+    
+}
+const project_description = {
+    "the-Book-fair" :"A book reviewing website. User can find books and their reviews",
+    "chat-application":"This application allows its user to communicate with each other through channels.",
+    "post-it":"PostIT is web application to find Handyman, Electrician or Plumber.",
+    "wikipedia":"Design of Wikipedia-like online encyclopedia. Uses Github Markdown syntax to write the contents of encyclopedia",
+    "social-network":"Design of Social Network website"
 
+}
+const project_tech_list = {
+    "the-Book-fair" :["Flask","Python","Html","CSS"],
+    "chat-application": ["Flask","Python","Html/CSS"],
+    "post-it":["Django","JavaScript", "Html/CSS"],
+    "wikipedia":["Django","Python","Python Regex"],
+    "social-network":["Django","Python","JavaScript"]
+}
 
 
 
@@ -13,7 +31,6 @@ const skillsPercentage = {
 
 
 }
-
 
 
 async function animatebarSection(){
